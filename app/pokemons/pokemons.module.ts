078a6668e9e28ec20159163dce1f85e06fd4aaf8
vanessa loviton <loviton.vanessa@yahoo.fr>
2017-09-13 09:06:08 -0700

    Refacto

---
+++ app/pokemons/pokemons.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ListPokemonComponent }   from './list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon.component';
+import { ShadowCardDirective }    from './shadow-card.directive';
+import { PokemonTypeColorPipe }   from './pokemon-type-color.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    ShadowCardDirective,
+    PokemonTypeColorPipe
+  ],
+  providers: []
+})
+export class PokemonsModule { }
