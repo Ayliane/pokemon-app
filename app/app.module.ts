4e75ea103b8abcac7d2c10ddd644c4e80faab8a1
vanessa loviton <loviton.vanessa@yahoo.fr>
2017-09-19 05:47:19 -0700

    Import rxjs

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
+import './rxjs-extensions';
 
 import { PokemonsModule } from './pokemons/pokemons.module';
 import { PageNotFoundComponent } from './page-not-found.component';
